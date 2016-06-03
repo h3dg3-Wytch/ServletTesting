4af62dad4b05bbef33db4f498371ba655ae109fa
Rex <rex.petersen@onlineinsight.com>
2016-06-03 12:28:41 -0400

    Set up, hello world servlet

---
+++ src/main/java/com/h3dg3Wytch/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.h3dg3Wytch;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by rex on 6/3/16.
+ */
+public class HelloWorld extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+    }
+
+
+}
